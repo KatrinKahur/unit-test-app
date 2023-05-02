e2ae57fec2a145ba73908ddf842600315179d833
Jennifer <je8720su-s@student.lu.se>
2023-05-02 22:22:26 +0200

    update design

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -2,6 +2,7 @@ import handleFile from "../utils/fileHandler";
 import React from "react";
 import getResponse from "../api/apiHandler";
 import generatePrompt from "../utils/promptHandler";
+import '../css/App.css';
 
 function MainPage(){
     const [fileContent, setFileContent] = React.useState("");
@@ -15,14 +16,20 @@ function MainPage(){
         }
     }, [fileContent])
     return(
-        <>
-            <input type="file"
-                   id="file-input"
-                   onChange={(event) => handleFile(event, setFileContent)}/>
-            <h3>Response:</h3>
-            <pre>{response}</pre>
-        </>
+        <div>
+            <h1 className="toolName">Java Unit Test Generation AI (JUTGAI)</h1>
+            {/*Generation Of Unit Test In Java (GOUTIJ)*/}
+            <div className="inputButt">
+                <input type="file"
+                    id="file-input"
+                    onChange={(event) => handleFile(event, setFileContent)}/>
+            </div>
+            <h3 className="responseTitle">Response:</h3>
+            <div className="responseBlock">
+                <pre>{response}</pre>
+            </div>
+        </div>
     )
-}
+} 
 
 export default MainPage;
