d7569fd744935a413d22dad04cf3fa4dacc7f17d
Katrin Kahur <katrin.kahur@gmail.com>
2023-05-22 11:20:35 +0200

    Added error message

---
+++ src/components/errorMessage.js
@@ -0,0 +1,18 @@
+import {Alert} from "react-bootstrap";
+import React from "react";
+
+export const ErrorMessage = ({message, show, setShow}) => {
+
+    if(message === null){
+        return null
+    }
+
+    if(show){
+        return (
+            <Alert data-testid="error-message" style={{position: "fixed", top:10, left: 0, right: 0, zIndex: 999}} variant="danger" onClose={() => setShow(false)} dismissible>
+                <Alert.Heading>Error!</Alert.Heading>
+                <p>{message}</p>
+            </Alert>
+        );
+    }
+}
